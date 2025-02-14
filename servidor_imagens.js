4b6985fbc5210be6864f027ce02d50cb3ab74efa
Pedro Vasconcelos <151564394+Pedro-H-Avila@users.noreply.github.com>
2025-02-14 11:53:00 -0300

    Add files via upload

---
+++ servidor_imagens.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+
+const imagens = [
+  'garfield1.jpg',
+  'garfield2.png',
+  'garfield3.gif',
+  'garfield4.jpg',
+  'garfield8.png'
+];
+
+let indiceAtual = 0;
+
+// Endpoint que retorna o próximo caminho da imagem
+app.get('/imagens', (req, res) => {
+  const imagemAtual = imagens[indiceAtual];
+  indiceAtual = (indiceAtual + 1) % imagens.length;
+  res.json({ url: imagemAtual });
+});
+
+// Endpoint que retorna a primeira imagem do tipo PNG
+app.get('/imagens/png', (req, res) => {
+  const imagemPng = imagens.find((img) => img.endsWith('.png'));
+  res.json({ url: imagemPng });
+});
+
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
